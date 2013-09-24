cfd485bbb51b187858ffd26b3009c02297a050ea
Antonio Ricardo <antonioricardo@ifollowagencia.com.br>
2013-09-24 15:12:13 -0400

    assets active admin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Pdtms::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.precompile += %w[active_admin.css active_admin.js]
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
