eb9d3d6930005fcfd53cd0f34f11e53b508544a8
Antonio Ricardo <antonioricardo@ifollowagencia.com.br>
2013-09-27 01:04:17 -0400

    static assets server

--- app/models/newss.rb
+++ app/models/newss.rb
@@ -1,5 +1,5 @@
 class Newss < ActiveRecord::Base
-	 mount_uploader :imagenews, ImageNewsUploader
+  mount_uploader :imagenews, ImageNewsUploader
   attr_accessible :author, :content, :date, :description, :title, :imagenews
   
 end
